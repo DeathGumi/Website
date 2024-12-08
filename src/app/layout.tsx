9d4d86508c1381009dade6889828960d789b8e13
DeathGumi <dueldnh@gmail.com>
2024-12-08 01:11:53 -0800

    got rid of padding that interupted with the actual layout being pushed to the rgiht

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
       >
         <ThemeProvider>
           <Nav />  
-          <main className="pl-64"> 
+          <main>
             {children}
           </main>
         </ThemeProvider>
