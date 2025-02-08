b9f72aa2f039372faa58f724df05c929e5741857
DeathGumi <dueldnh@gmail.com>
2025-02-08 00:08:01 -0800

    Simple fix decided in order to fix size i set the size of iamges to specific and only to show three since there are more images inside that you could further click and keep a consistent layout

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -352,16 +352,17 @@ const ProjectsSection: React.FC = () => {
 
                     {project.images && project.images.length > 0 && (
                       <div className="grid grid-cols-3 gap-2 mb-4">
-                        {project.images.map((image, i) => (
+                        {project.images.slice(0, 3).map((image, i) => (
                           <div 
                             key={i} 
                             className="relative rounded-lg overflow-hidden bg-blue-900/30 group/image cursor-pointer"
                             onClick={() => setSelectedImage({ projectIndex: index, imageIndex: i })}
+                            style={{ paddingBottom: '56.25%' }} 
                           >
                             <img
                               src={image.src}
                               alt={image.title}
-                              className="w-full h-full object-cover transition-transform duration-300 group-hover/image:scale-105"
+                              className="absolute inset-0 w-full h-full object-cover transition-transform duration-300 group-hover/image:scale-105"
                             />
                           </div>
                         ))}
